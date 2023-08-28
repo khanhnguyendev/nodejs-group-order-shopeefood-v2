71216831277ddd446ae8998a3d9399d614cbf01a
Ngo Khanh Nguyen <khanhnguyen.vlu@gmail.com>
2023-08-28 11:54:20 +0700

    remove config

--- config/config.js
+++
@@ -1,5 +0,0 @@
-module.exports = {
-  db: {
-    uri: "mongodb+srv://khanhnguyendev:Khanhnguyen97@groupordershopeefood.gjgtfwr.mongodb.net/groupordershopee?retryWrites=true&w=majority", //place the uri of your mongoDB database here
-  },
-};
